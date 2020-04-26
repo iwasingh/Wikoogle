0b6cfd11e7332e4be27268ff483e1e6910d257b5
Matteo Guerzoni <guerzonim97@gmail.com>
2020-04-26 11:09:15 -0500

    view

--- src/main.py
+++ src/main.py
@@ -4,21 +4,17 @@ from pathlib import Path
 # from preprocessing import index
 from preprocessing.index import WikiIndex
 from searching.searcher import Searcher
+from flask import Flask, request
 
 
-def main():
-    wikimedia_ix = WikiIndex().get('__index')
-    # wikimedia_ix.build()
+wikimedia_ix = WikiIndex().get('__index')
 
-    searcher = Searcher(wikimedia_ix)
+searcher = Searcher(wikimedia_ix)
 
-    while True:
-        query = input('Insert query \n')
-        if query == 'exit':
-            break
+app = Flask(__name__)
 
-        results = searcher.search(query)
+@app.route('/search')
+def search_results():
+    print(searcher.search('anar'))
+    return 'Ok'
 
-
-if __name__ == '__main__':
-    main()
